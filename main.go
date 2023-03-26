392bc587d9e61344be5f5d5fb061f22e1297e011
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-03-26 11:21:19 +0900

    echoの導入

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "hello world")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
